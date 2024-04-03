881d1e29da3d3a1da515529f141fe094a94fccfb
lsw <hkbvc1222@gmail.com>
2024-04-03 13:58:25 +0900

    Update vercel

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,16 @@
-import config from "./config";
-import * as auth from "./controller/auth";
-import * as gameList from "./controller/gameList";
-import * as point from "./controller/point";
-import { start } from "./express";
-import * as DB from "./util/DB";
+const express = require("express");
 
-async function databaseInit() {
-  // DROP DATABASE IF EXISTS slot;
-  const result = await DB.query(`
-  CREATE DATABASE IF NOT EXISTS slot;
-  use slot;
-  `);
+const app = express();
+const PORT = 8000;
 
-  await gameList.initDB();
-  await auth.initDB();
+app.get("/", (req: any, res: { send: (arg0: string) => void }) => {
+  res.send("Hello World");
+});
 
-  const adminId = "admin";
-  await auth.signUp(adminId, "1234", 100);
-  await auth.signUp("lucy", "1234");
+app.get("/about", (req: any, res: { send: (arg0: string) => void }) => {
+  res.send("About route 🎉 ");
+});
 
-  await point.initDB();
-  await point.charge(adminId, 500);
-  await point.charge(adminId, 500);
-  await point.charge(adminId, 500);
-  await point.charge(adminId, 500);
-  await point.charge(adminId, 500);
-}
-const main = async () => {
-  const dbConnected = await DB.init(config.dbConfig);
-  console.log("DB connected:", dbConnected);
-  await databaseInit();
-
-  const app = await start();
-
-  // @kwonth1210 :  서버를 이곳에서 가동시킬수 있도록 수정. 모든 모듈의 실행 여부는 이곳에서 제어하도록 작성.( 실행하지않을시 오작동 하지않도록 구성할것. )
-  // const sockIOServer = await startSocketIOServer(app);
-};
-main();
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on port ${PORT}`);
+});
