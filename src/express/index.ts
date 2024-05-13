d5965eb204dfbd89eb542c5212a63864cbe44375
lsw <sonid.iot.dev@gmail.com>
2024-05-13 10:18:00 +0900

    Update

--- src/express/index.ts
+++ src/express/index.ts
@@ -1,36 +1,11 @@
-import cors from "cors";
 import express from "express";
 const listenPort = 3000;
 const app = express();
 
-function mainRouteConfig(app: express.Express) {
-  app.use(
-    express.json({
-      limit: "50mb",
-    })
-  );
-  app.use(
-    express.urlencoded({
-      limit: "50mb",
-      extended: true,
-    })
-  );
-  app.use(
-    cors({
-      origin: true,
-      credentials: true,
-    })
-  );
-}
-
-import router from "./route";
-
 async function start() {
   // 순서 주의
-  mainRouteConfig(app);
 
   app.use(express.static("/"));
-  app.use("/api", router);
 
   app.listen(listenPort, async () => {
     const env =
